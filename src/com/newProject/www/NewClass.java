31d4a4cc952ad01317acea2d7dcb4058d47a15d9
John <john@a.com>
2024-10-06 13:42:27 -0400

    added text to the tempFile.txt

---
+++ src/com/newProject/www/NewClass.java
@@ -0,0 +1,21 @@
+package com.newProject.www;
+
+public class NewClass {
+	
+public void abc() {
+	
+
+System.out.println("abc");
+
+}
+
+public static void main(String[] args) {
+	
+}
+	
+	
+	
+
+}
+
+
